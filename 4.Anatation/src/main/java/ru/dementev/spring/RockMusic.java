df49cc7aafc269ddd336c032b24d792385029375
Slava Dementev <kursant344@gmail.com>
2023-03-20 13:28:06 +0300

    add @Component

--- 4.Anatation/src/main/java/ru/dementev/spring/RockMusic.java
+++ 4.Anatation/src/main/java/ru/dementev/spring/RockMusic.java
@@ -1,5 +1,8 @@
 package ru.dementev.spring;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class RockMusic implements Music{
     @Override
     public String getSong() {
--- 4.Anatation/src/main/java/ru/dementev/spring/TestSpring.java
+++ 4.Anatation/src/main/java/ru/dementev/spring/TestSpring.java
@@ -8,24 +8,13 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        ClassicMusic classicMusic = context.getBean("musicBean", ClassicMusic.class);
-        System.out.println(classicMusic.getSong());
+        Music music = context.getBean( "rockMusic", Music.class);
+
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        musicPlayer.playMusic();
 
 
-       /* MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-        System.out.println(comparison);
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-
-        firstMusicPlayer.setVolume(10);
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());*/
-
-        //System.out.println(musicPlayer.getName());
-        //System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
